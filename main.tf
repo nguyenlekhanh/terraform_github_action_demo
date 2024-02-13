25cbb8b9a54cd146ad0b6d910822874e6b123b71
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-02-13 12:07:35 -0600

    remove profile in provider

--- main.tf
+++ main.tf
@@ -18,8 +18,6 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "aws"
 }
 
 module "tf-state" {
